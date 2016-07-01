37984312a1702cbb5552ead1d54067015efb638f
Thierry Templier <templth@yahoo.fr>
2016-07-01 21:18:11 +0200

    Initial import

---
+++ webpack.config.hot.reload.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var PUBLIC_PATH = 'http://localhost:8080';
+
+var config = {
+  entry: [
+    'webpack-dev-server/client?' + PUBLIC_PATH,
+    'webpack/hot/only-dev-server',
+    APP_DIR + '/index.jsx'
+  ],
+  output: {
+    path: BUILD_DIR,
+    //publicPath: 'http://localhost:8080/',
+    filename: 'bundle.js',
+    publicPath: PUBLIC_PATH + "/src/client/"
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+      	loaders: ['react-hot', 'babel']
+        //loader : 'babel'
+      },
+      /*{
+      	test: /\.jsx?$/,
+      	//loaders: ['react-hot', 'jsx?harmony'],
+      	loaders: ['react-hot', 'babel'],
+      	include: APP_DIR
+      }*/
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+module.exports = config;
